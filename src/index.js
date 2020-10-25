9f390a65d980a4b9cd7ebf6180883423aadc5d27
plyolik <p.lyolik@mail.ru>
2020-10-25 18:57:21 +0400

    faet: resolve task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+var BreackException = {}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let firsts = bracketsConfig.map(c => c[0])
+  let lasts = bracketsConfig.map(c => c[1])
+  const holder = []
+  for (let letter of str) {
+    state = 0
+    if (firsts.includes(letter) && lasts.includes(letter)) {
+      state = holder.filter(h => h === letter).length % 2 === 0 ? 1 : 2
+    }
+    else if (firsts.includes(letter)) {
+      state = 1
+    } else if (lasts.includes(letter)) {
+      state = 2
+    }
+    if (state === 1) {
+      holder.push(letter)
+    } else if (state === 2) {
+      const openPair = firsts[lasts.indexOf(letter)]
+      if (holder[holder.length - 1] === openPair) {
+          holder.splice(-1,1)
+      } else {
+          holder.push(letter)
+          break
+      }
+    }
+  }
+
+  return holder.length === 0
 }
